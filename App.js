8c759ce5a812699716e408e0223788d0c4273cd7
Begüm Doğru <53992127+begumdogru@users.noreply.github.com>
2022-07-04 01:17:30 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MainApp from "./src/Main";
+import { NativeBaseProvider } from "native-base";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <MainApp />
+    </NativeBaseProvider>
+  );
+}
